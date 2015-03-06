4d2d83c07aba13694a6a4579830eb7ab83a2fae7
郑 宏 <zhenghong596@hong.local>
2015-03-06 23:29:38 +0800

    模糊背景＋切换传值

--- wangwang/wangwang/mainContainer.swift
+++ wangwang/wangwang/mainContainer.swift
@@ -10,44 +10,22 @@ import UIKit
 
 class mainContainer: UIViewController {
 
-    var blurView = UIImageView()
+
+    @IBOutlet weak var img: UIImageView!
     var app = UIApplication.sharedApplication().delegate as AppDelegate
     
-//    @IBOutlet weak var container: UIView!
-    
-    @IBAction func gotoLeft(sender: AnyObject) {
+    func updateBlur(blurImg: UIImageView) {
         
-        updateBlur()
-    }
-//    @IBAction func backMain(sender: AnyObject) {
-//        container.hidden = true
-//        
-//    }
-    
-    func updateBlur() {
-        
-        //为了避免在截图的时候截到选项界面
-        //container.hidden = true
-        
-        //创建一个新的ImageContext来绘制截图，你没有必要去渲染一个完整分辨率的高清截图，使用ImageContext可以节约掉不少的计算量
         UIGraphicsBeginImageContextWithOptions(self.view.bounds.size, true, 1)
-        
-        //将界面绘制到ImageContext中去，因为你需要确保选项界面是隐藏状态因此你需要等待屏幕刷新后才能绘制
         self.view.drawViewHierarchyInRect(self.view.bounds, afterScreenUpdates: true)
-        
-        //将ImageContext放入一个UIImage内然后清理掉这个ImageContext
+
         let screenshot = UIGraphicsGetImageFromCurrentImageContext()
 
         UIGraphicsEndImageContext()
         
-        let blur = screenshot.applyLightEffect()
-
-        let iv = UIImageView(image: blur)
-        iv.frame = CGRectMake(0, 0, 400, 640)
-        view.addSubview(iv)
-        //app.showMask(blur)
-    
-        //container.hidden = false
+        let blur = screenshot.applyDarkEffect()
+  
+        blurImg.image = blur
     }
     
     override func viewDidLoad() {
@@ -61,15 +39,27 @@ class mainContainer: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        let des = segue.destinationViewController as left
+        updateBlur(des.imgView)
     }
     
-
+    @IBAction func back(segue: UIStoryboardSegue, sender: AnyObject?){
+        
+        let btnVc = segue.sourceViewController as left
+        
+        let title = btnVc.btnT
+        if(title == "test1"){
+            img.image = UIImage(named: "photo-15.jpg")
+        } else if(title == "test2") {
+            img.image = UIImage(named: "photo-16.jpg")
+        } else {
+            img.image = UIImage(named: "photo-17.jpg")
+        }
+    }
+    
 }
